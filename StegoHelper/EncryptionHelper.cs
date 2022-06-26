674df1e8ef237b9c4079c8af8c022b289a1d01e7
hardrive9000 <78927348+hardrive9000@users.noreply.github.com>
2022-06-26 17:42:25 -0300

    Initial commit
    Initial commit

---
+++ StegoHelper/EncryptionHelper.cs
@@ -0,0 +1,104 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace StegoHelper
+{
+    public static class AESEncryptionHelper
+    {
+        /// <summary>
+        /// Encrypts input text using AES-256 encryption.
+        /// </summary>
+        /// <param name="clearMessage">Clear unencrypted text.</param>
+        /// <param name="password">Password that you want to use to encrypt the text.</param>
+        /// <returns>Returns (Base64 encoded) encrypted AES-256 text.</returns>
+        public static string Encrypt(string clearMessage, string password)
+        {
+            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+            // so that the same Salt and IV values can be used when decrypting.
+            byte[] saltStringBytes = Generate128BitsOfRandomEntropy();
+            byte[] ivStringBytes = Generate128BitsOfRandomEntropy();
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(clearMessage);
+            using (Rfc2898DeriveBytes derivedPassword = new(password, saltStringBytes, 1000))
+            {
+                byte[] keyBytes = derivedPassword.GetBytes(32); //256bits
+                using (Aes symmetricKey = Aes.Create())
+                {
+                    symmetricKey.BlockSize = 128;
+                    using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                    {
+                        using (MemoryStream memoryStream = new())
+                        {
+                            using (CryptoStream cryptoStream = new(memoryStream, encryptor, CryptoStreamMode.Write))
+                            {
+                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                cryptoStream.FlushFinalBlock();
+                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+                                byte[] cipherTextBytes = saltStringBytes;
+                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                memoryStream.Close();
+                                cryptoStream.Close();
+
+                                return Convert.ToBase64String(cipherTextBytes);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypts input AES-256 encrypted (Base64 encoded) text.
+        /// </summary>
+        /// <param name="encryptedMessage">Encrypted AES-256 (Base64 encoded) text.</param>
+        /// <param name="password">Password that you want to use to decrypt the text.</param>
+        /// <returns>Returns decrypted plain text.</returns>
+        public static string Decrypt(string encryptedMessage, string password)
+        {
+            // Get the complete stream of bytes that represent:
+            // [16 bytes of Salt] + [16 bytes of IV] + [n bytes of CipherText]
+            byte[] cipherTextBytesWithSaltAndIv = Convert.FromBase64String(encryptedMessage);
+            // Get the saltbytes by extracting the first 16 bytes from the supplied cipherText bytes.
+            byte[] saltStringBytes = cipherTextBytesWithSaltAndIv.Take(16).ToArray();
+            // Get the IV bytes by extracting the next 16 bytes from the supplied cipherText bytes.
+            byte[] ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(16).Take(16).ToArray();
+            // Get the actual cipher text bytes by removing the first 32 bytes from the cipherText string.
+            byte[] cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip(32).Take(cipherTextBytesWithSaltAndIv.Length - 32).ToArray();
+
+            using (Rfc2898DeriveBytes derivedPassword = new(password, saltStringBytes, 1000))
+            {
+                byte[] keyBytes = derivedPassword.GetBytes(32); //256bits
+                using (Aes symmetricKey = Aes.Create())
+                {
+                    symmetricKey.BlockSize = 128;
+                    using (ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                    {
+                        using (MemoryStream memoryStream = new(cipherTextBytes))
+                        {
+                            using (CryptoStream cryptoStream = new(memoryStream, decryptor, CryptoStreamMode.Read))
+                            {
+                                using (StreamReader plainTextReader = new(cryptoStream))
+                                {
+                                    return plainTextReader.ReadToEnd();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static byte[] Generate128BitsOfRandomEntropy()
+        {
+            byte[] randomBytes = new byte[16]; // 16 Bytes will give us 128 bits.
+
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                // Fill the array with cryptographically secure random bytes.
+                rng.GetBytes(randomBytes);
+            }
+
+            return randomBytes;
+        }
+    }
+}
---
+++ StegoHelper/SteganographyHelper.cs
@@ -0,0 +1,235 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace StegoHelper
+{
+    public static class SteganographyHelper
+    {
+        private enum State
+        {
+            Hiding,
+            FillingWithZeros
+        };
+
+        /// <summary>
+        /// Embed text (clear or encrypted) into an image.
+        /// </summary>
+        /// <param name="text">Clear or encrypted text to embed.</param>
+        /// <param name="bmp">Image in which the text should be embedded.</param>
+        /// <returns>Returns an image with embedded text.</returns>
+        public static Image EmbedText(string text, Image<Rgba32> bmp)
+        {
+            // initially, we'll be hiding characters in the image
+            State state = State.Hiding;
+
+            // holds the index of the character that is being hidden
+            int charIndex = 0;
+
+            // holds the value of the character converted to integer
+            int charValue = 0;
+
+            // holds the index of the color element (R or G or B) that is currently being processed
+            long pixelElementIndex = 0;
+
+            // holds the number of trailing zeros that have been added when finishing the process
+            int zeros = 0;
+
+            // hold pixel elements
+            byte R, G, B;
+
+            // pass through the rows
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                // pass through each row
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    // holds the pixel that is currently being processed
+                    Rgba32 pixel = bmp[j, i];
+
+                    // now, clear the least significant bit (LSB) from each pixel element
+                    R = (byte)(pixel.R - pixel.R % 2);
+                    G = (byte)(pixel.G - pixel.G % 2);
+                    B = (byte)(pixel.B - pixel.B % 2);
+
+                    // for each pixel, pass through its elements (RGB)
+                    for (int n = 0; n < 3; n++)
+                    {
+                        // check if new 8 bits has been processed
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            // check if the whole process has finished
+                            // we can say that it's finished when 8 zeros are added
+                            if (state == State.FillingWithZeros && zeros == 8)
+                            {
+                                // apply the last pixel on the image
+                                // even if only a part of its elements have been affected
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp[j, i] = Color.FromRgba(R, G, B, 255);
+                                }
+
+                                // return the bitmap with the text hidden in
+                                return bmp;
+                            }
+
+                            // check if all characters has been hidden
+                            if (charIndex >= text.Length)
+                            {
+                                // start adding zeros to mark the end of the text
+                                state = State.FillingWithZeros;
+                            }
+                            else
+                            {
+                                // move to the next character and process again
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        // check which pixel element has the turn to hide a bit in its LSB
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        // the rightmost bit in the character will be (charValue % 2)
+                                        // to put this value instead of the LSB of the pixel element
+                                        // just add it to it
+                                        // recall that the LSB of the pixel element had been cleared
+                                        // before this operation
+                                        R = (byte)(R + charValue % 2);
+
+                                        // removes the added rightmost bit of the character
+                                        // such that next time we can reach the next one
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G = (byte)(G + charValue % 2);
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B = (byte)(B + charValue % 2);
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp[j, i] = Color.FromRgba(R, G, B, 255);
+                                }
+                                break;
+                        }
+
+                        pixelElementIndex++;
+
+                        if (state == State.FillingWithZeros)
+                        {
+                            // increment the value of zeros until it is 8
+                            zeros++;
+                        }
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+        /// <summary>
+        /// Extracts text (clear or encrypted) from an image.
+        /// </summary>
+        /// <param name="bmp">Image in which the text is embedded.</param>
+        /// <returns>Returns clear or encrypted text from an image.</returns>
+        public static string ExtractText(Image<Rgba32> bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            // holds the text that will be extracted from the image
+            string extractedText = string.Empty;
+
+            // pass through the rows
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                // pass through each row
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Rgba32 pixel = bmp[j, i];
+
+                    // for each pixel, pass through its elements (RGB)
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    // get the LSB from the pixel element (will be pixel.R % 2)
+                                    // then add one bit to the right of the current character
+                                    // this can be done by (charValue = charValue * 2)
+                                    // replace the added bit (which value is by default 0) with
+                                    // the LSB of the pixel element, simply by addition
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+
+                        colorUnitIndex++;
+
+                        // if 8 bits has been added, then add the current character to the result text
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            // reverse? of course, since each time the process happens on the right (for simplicity)
+                            charValue = ReverseBits(charValue);
+
+                            // can only be 0 if it is the stop character (the 8 zeros)
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+
+                            // convert the character value from int to char
+                            char c = (char)charValue;
+
+                            // add the current character to the result text
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        private static int ReverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+    }
+}
